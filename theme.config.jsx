7f6809e0d254255733bd29b53eda37817cb4c5f1
vyogami <shiv.05cs@gmail.com>
2023-07-11 02:41:03 +0530

    feat(nextra): initialize empty nextra project

---
+++ theme.config.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+ 
+
+
+export default {
+  logo: (
+    <>
+      <Image src="/logos/logo.png" alt="logo" width={160} height={40} />
+    </>
+  ),
+  project: {
+    link: "https://github.com/vyogami/asoc",
+  },
+  docsRepositoryBase: 'https://github.com/vyogami/asoc/blob/main'
+  
+};
